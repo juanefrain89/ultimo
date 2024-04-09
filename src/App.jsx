b0a148fa96ebd4d1619d579dc690542322adda4d
juanefrain89 <163329535+juanefrain89@users.noreply.github.com>
2024-04-08 20:35:14 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
             <I18nextProvider i18n={i18next}>
                 <BrowserRouter>
                     <Routes>
-                        <Route path='/' element={<Register />} />
+                        <Route path='/' element={<Registrar />} />
                        <Route path='/registrar' element={<Registrar/>} />
                         {isLoggedIn ? (
                             <>
